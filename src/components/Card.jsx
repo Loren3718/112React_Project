05108b1c26449639efee20e234b5c6cf83ee4e06
LorenLap <B11117021@gemail.yuntech.edu.tw>
2024-05-31 16:38:18 +0800

    Updata /Components/TDX-API

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+/**
+ * @author Loren Huang <LorenHu3718@gmail.com>
+ */
+
+export default function CustomCard({item})
+{
+    return (
+        <div class="border">
+            <div>{item.ScenicSporName}</div>
+        </div>
+    )
+}
